20932ef97fdd2a4d7a2a973c86ccc99e1c25939d
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-05-05 00:27:04 +0300

    outputting a list of meetups

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup"
+  }, 
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 6, 12345 Some City",
+    description: "This is a second meetup"
+  }
+]
+
 export default function HomePage() {
-  return <h1>Default page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
