ee683b8c1fdea41c6f058e462ad8d27ebebb559f
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-06-03 15:27:15 +0300

    add getStaticPaths

--- src/pages/[meetupId].js
+++ src/pages/[meetupId].js
@@ -3,14 +3,36 @@ import MeetupDetail from "@/components/meetups/MeetupDetail";
 function MeetupDetails() {
   return (
     <MeetupDetail
-      image=
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
-      
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
       title="A Fist Meetup"
       address="Some Street 5, 12345 Some City"
       description="This is a first meetup"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m2" } }],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "A Fist Meetup",
+        id: meetupId,
+        address: "Some Street 5, 12345 Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
