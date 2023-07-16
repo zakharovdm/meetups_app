45700599250ab01a8f4cc138272c9b786cb12daf
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-07-16 22:14:58 +0300

    feat: create basic api on mongoDB

--- src/pages/api/new-meetup.js
+++ src/pages/api/new-meetup.js
@@ -1,7 +1,19 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect("mongodb+srv://dzaharov:sIr4VrvGbB7NPhDZ@cluster0.qbjtqnv.mongodb.net/meetups?retryWrites=true&w=majority")
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
